33eb7275caea607673064dc4adaaa0c22e6d319a
Jorge Serrato <jorgevserrato@gmail.com>
2025-04-11 07:45:44 -0600

    Actualizacion: Control de servo

--- LecturaMultiplesSensores_bmp280_dht22/LecturaMultiplesSensores_bmp280_dht22.ino
+++ LecturaMultiplesSensores_bmp280_dht22/LecturaMultiplesSensores_bmp280_dht22.ino
@@ -10,14 +10,12 @@ Objetivo: Aprender a conectar y leer datos de múltiples sensores I2C (DHT22 y B
 
 #include <Wire.h>
 #include "Adafruit_Sensor.h"
-#include "Adafruit_BMP280.h"
 #include "DHT.h"
 
 #define DHTPIN 2       // Pin digital conectado al DHT22
 #define DHTTYPE DHT22  // Tipo de sensor DHT
 
 DHT dht(DHTPIN, DHTTYPE);
-Adafruit_BMP280 bmp;  // Objeto BMP280
 
 void setup() {
   Serial.begin(9600);
@@ -37,20 +35,10 @@ void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
-  // Lectura del BMP280
-  float temp = bmp.readTemperature();
-  float pressure = bmp.readPressure();
-
   // Mostrar datos en el Monitor Serial
   Serial.print(F("Humedad: "));
   Serial.print(h);
   Serial.print(F("%  Temperatura (DHT22): "));
   Serial.print(t);
   Serial.println(F("°C"));
-
-  Serial.print(F("Temperatura (BMP280): "));
-  Serial.print(temp);
-  Serial.print(F("°C  Presión: "));
-  Serial.print(pressure / 100.0F);
-  Serial.println(F("hPa"));
 }
